33d6cef79868bd4b34135671c3f746561cc52fa5
Samuel Oliver <sam.xyz@outlook.com>
2023-08-23 10:34:08 +0000

    Added config.json for 189b build

--- startup-convert.sh
+++ startup-convert.sh
@@ -6,7 +6,7 @@
 # when filling out the teams provisioning details, use -1 for optional integers, empty quotes "" for optional strings.
 
 TEAM_ID="lastpass"
-TEAM_KEY="lastpass"   
+TEAM_KEY="tapi_2UNiDcE5knOpfq7Wyslt8Wdn42T"   
 APP_RULE_ID="tar_2KaPxe720b5CNKhy6ii6VHDtNtX"
 GUEST_ACCESS="0"
 USER_ID="-1"
@@ -17,7 +17,7 @@ USER_EMAIL='""'
 curl https://builds.parsecgaming.com/package/parsec-macos-startup.pkg -o "/Users/Shared/parsec-macos-startup.pkg"
 sleep 1
        
-BINARY_DIRECTORY="/Users/Shared/"
+BINARY_DIRECTORY="Users/remote/Downloads/TeamsInstall"
 INSTALL_PATH="/Users/Shared/.parsec"
 
 
@@ -27,7 +27,7 @@ then
  launchctl unload -w /Library/LaunchAgents/com.parsec.app.plist
 fi
 sudo chmod 755 $BINARY_DIRECTORY/teams
-sudo installer -pkg $BINARY_DIRECTORY/parsec-macos-startup.pkg -target /Applications
+sudo installer -pkg /Users/Shared/parsec-macos-startup.pkg -target /Applications
 launchctl load -w /Library/LaunchAgents/com.parsec.app.plist 
 until [ $(ls $INSTALL_PATH/ | grep -c config.txt) -gt 0 ]
 do 
@@ -37,7 +37,7 @@ launchctl unload -w /Library/LaunchAgents/com.parsec.app.plist
 sudo xattr -r -d com.apple.quarantine $BINARY_DIRECTORY/teams
 sudo $BINARY_DIRECTORY/teams kessel-api.parsec.app $TEAM_KEY $TEAM_ID $APP_RULE_ID $GUEST_ACCESS $USER_ID $TEAM_GROUP_ID $USER_EMAIL "$INSTALL_PATH/user.bin" 
 echo 'app_channel=client_multiuser' >> "$INSTALL_PATH/config.txt"
-echo 'app_host=1' >> "$INSTALL_PATH/config.txt"
+echo 'app_host=1' >> "$INSTALL_PATH/config.json"
 sudo rm -r /Users/$USER/Library/Caches/tv.parsec.www 
 launchctl load -w /Library/LaunchAgents/com.parsec.app.plist 
 until [ $(ls $INSTALL_PATH/ | grep -c client_multiuser) -gt 0 ]
